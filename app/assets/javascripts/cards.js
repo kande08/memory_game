61fc4ae1ab7af19d752b4d02a54651e00b866f34
Kaitlin Anderson <kaitlin_anderson@mac.com>
2015-12-16 19:59:05 -0700

    cannot click on same boxes twice, randomized numbers

--- app/assets/javascripts/cards.js
+++ app/assets/javascripts/cards.js
@@ -2,16 +2,30 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
   var clickCount = 0;
+  var array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
   // show card on click,
   $('.box').click(function(){
-    $(this).children(0).removeClass('hide').addClass('clicked');
-    clickCount++;
-    checkMatches();
-    wonGame();
+    if($(this).children(0).hasClass('hide')) {
+      $(this).children(0).removeClass('hide').addClass('clicked');
+      clickCount++;
+      checkMatches();
+      wonGame();
+    }
+  });
+
+  $('#new_game').click(function(){
+    array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+    var shuffledArray = shuffle(array);
+    $('.box').each(function(){
+      $(this).children(0).text(shuffledArray.pop())
+    });
+      clickCount = 0;
+      $('p').addClass('hide');
   });
   // limit it to two cards, determine if it's a match, have alerts for if its a match or not.
   // if not a match, reset class to hide
   // once all items are matched, announce won game
+  // don't allow matched items to be clicked again, or clicked items to be clicked again
 
   function checkMatches() {
     //when clickCount is 2, check for a match
@@ -39,9 +53,27 @@ $(document).ready(function(){
       // keep playing
     } else {
       alert('You WON!!!!!!!!');
-      clickCount = 0;
-      $('p').addClass('hide');
     }
+  }
+
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
 
